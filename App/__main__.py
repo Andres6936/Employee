5c2a7dd6b240a32d3c9f1da64d5d9f4675835513
Andres6936 <andres6936@gmail.com>
2023-06-14 08:55:43 -0500

    Load From Enviroment Variables

--- App/__main__.py
+++ App/__main__.py
@@ -1,10 +1,14 @@
 import sys
 
+from dotenv import load_dotenv
 from PyQt6.QtWidgets import QApplication
 
 from App.LoginWindow import LoginWindow
 
 if __name__ == '__main__':
+    # Take environment variables from .env
+    load_dotenv()
+
     app = QApplication(sys.argv)
     window = LoginWindow()
     sys.exit(app.exec())
