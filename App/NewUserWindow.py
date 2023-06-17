56b3dc7d56f4afc6cc7d93b0c49319cce6aa94d4
Andres6936 <andres6936@gmail.com>
2023-06-17 09:43:59 -0500

    Added FormLayout

--- App/NewUserWindow.py
+++ App/NewUserWindow.py
@@ -1,6 +1,6 @@
 from PyQt6.QtCore import Qt, QRegularExpression
 from PyQt6.QtGui import QFont, QRegularExpressionValidator
-from PyQt6.QtWidgets import QLabel, QLineEdit, QPushButton, QMessageBox, QWidget, QVBoxLayout, QHBoxLayout
+from PyQt6.QtWidgets import QLabel, QLineEdit, QPushButton, QMessageBox, QWidget, QVBoxLayout, QFormLayout
 
 
 class NewUserWindow(QWidget):
@@ -15,29 +15,11 @@ class NewUserWindow(QWidget):
         loginLabel.setAlignment(Qt.AlignmentFlag.AlignTop | Qt.AlignmentFlag.AlignHCenter)
         root.addWidget(loginLabel)
 
-        usernameLayout = QHBoxLayout()
-        usernameLabel = QLabel("Username:", self)
-        usernameLayout.addWidget(usernameLabel)
-
         self.usernameEdit = QLineEdit(self)
         self.usernameEdit.resize(250, 24)
-        usernameLayout.addWidget(self.usernameEdit)
-
-        root.addLayout(usernameLayout)
-
-        fullNameLayout = QHBoxLayout()
-        fullNameLabel = QLabel("Full Name:", self)
-        fullNameLayout.addWidget(fullNameLabel)
 
         fullNameEdit = QLineEdit(self)
         fullNameEdit.resize(250, 24)
-        fullNameLayout.addWidget(fullNameEdit)
-
-        root.addLayout(fullNameLayout)
-
-        emailLayout = QHBoxLayout()
-        emailLabel = QLabel("Email:", self)
-        emailLayout.addWidget(emailLabel)
 
         emailEdit = QLineEdit(self)
         emailEdit.setPlaceholderText("<username>@<domain>.com")
@@ -45,35 +27,31 @@ class NewUserWindow(QWidget):
             "\\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[com]{3}\\b",
             QRegularExpression.PatternOption.CaseInsensitiveOption)
         emailEdit.setValidator(QRegularExpressionValidator(regex))
-        emailLayout.addWidget(emailEdit)
-
-        root.addLayout(emailLayout)
-
-        newPasswordLayout = QHBoxLayout()
-        newPasswordLabel = QLabel("Password:", self)
-        newPasswordLayout.addWidget(newPasswordLabel)
 
         self.newPasswordEdit = QLineEdit(self)
         self.newPasswordEdit.setEchoMode(QLineEdit.EchoMode.Password)
         self.newPasswordEdit.resize(250, 24)
-        newPasswordLayout.addWidget(self.newPasswordEdit)
-
-        root.addLayout(newPasswordLayout)
-
-        confirmPasswordLayout = QHBoxLayout()
-        confirmLabel = QLabel("Confirm:", self)
-        confirmPasswordLayout.addWidget(confirmLabel)
 
         self.confirmEdit = QLineEdit(self)
         self.confirmEdit.setEchoMode(QLineEdit.EchoMode.Password)
         self.confirmEdit.resize(250, 24)
-        confirmPasswordLayout.addWidget(self.confirmEdit)
-
-        root.addLayout(confirmPasswordLayout)
 
         signUpButton = QPushButton("Sign Up", self)
         signUpButton.resize(320, 32)
         signUpButton.clicked.connect(self.onClickSignUp)
+
+        mainForm = QFormLayout()
+        mainForm.setFieldGrowthPolicy(mainForm.FieldGrowthPolicy.AllNonFixedFieldsGrow)
+        mainForm.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignTop)
+        mainForm.setLabelAlignment(Qt.AlignmentFlag.AlignRight)
+
+        mainForm.addRow("Username", self.usernameEdit)
+        mainForm.addRow("Fullname", fullNameEdit)
+        mainForm.addRow("Email", emailEdit)
+        mainForm.addRow("Password", self.newPasswordEdit)
+        mainForm.addRow("Confirm", self.confirmEdit)
+
+        root.addLayout(mainForm)
         root.addWidget(signUpButton)
 
         self.setLayout(root)
