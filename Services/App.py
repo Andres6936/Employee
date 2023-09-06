df864c7682c7ac756fffe4f73e55d1c442377d99
Andres6936 <andres6936@gmail.com>
2023-09-06 14:53:57 -0500

    Upload File to Storage

--- Services/App.py
+++ Services/App.py
@@ -1,12 +1,14 @@
 import asyncio
+import base64
 import os
 
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from hypercorn.asyncio import serve
 from hypercorn.config import Config
-from supabase_py import Client, create_client
+from supabase import Client, create_client
 
+from Services.Models.Document import Document
 from Services.Models.Quote import Quote
 from Services.Models.SignIn import SignIn
 from Services.Models.SignUp import SignUp
@@ -20,7 +22,10 @@ app = FastAPI()
 
 @app.post("/user/sign-up")
 def SignUp(signUp: SignUp):
-    response = supabase.auth.sign_up(signUp.email, signUp.password)
+    response = supabase.auth.sign_up({
+        "email": signUp.email,
+        "password": signUp.password
+    })
     return {
         'isBase64Encoded': False,
         'statusCode': 200,
@@ -30,7 +35,10 @@ def SignUp(signUp: SignUp):
 
 @app.post("/user/sign-in")
 def SignIn(signIn: SignIn):
-    response = supabase.auth.sign_in(signIn.email, signIn.password)
+    response = supabase.auth.sign_in_with_password({
+        "email": signIn.email,
+        "password": signIn.password
+    })
     return {
         'isBase64Encoded': False,
         'statusCode': 200,
@@ -63,9 +71,21 @@ def CreateQuote(create: Quote):
         }
 
 
-@app.post("/user/documents/upload/obligatory")
-def UploadObligatoryDocuments():
-    pass
-
+@app.post("/user/documents/obligatory")
+def UploadObligatoryDocuments(document: Document):
+    response = (supabase.storage.from_('DocumentsQuote')
+                .upload('Dummy.PDF', base64.b64decode(document.Base64)))
+    if response.status_code == 200:
+        return {
+            'isBase64Encoded': False,
+            'statusCode': 200,
+            'body': 'Successful'
+        }
+    else:
+        return {
+            'isBase64Encoded': False,
+            'statusCode': 403,
+            'body': response
+        }
 
 asyncio.run(serve(app, Config.from_mapping(use_reloader=True)))
