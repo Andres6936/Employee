0c937061c8f59eaca8b5e33154c2fa75a355bce6
Andres6936 <andres6936@gmail.com>
2023-09-10 13:28:35 -0500

    Added Documentation

--- Services/States/QuoteStates.py
+++ Services/States/QuoteStates.py
@@ -3,7 +3,13 @@ from enum import Enum, unique, auto
 
 @unique
 class QuoteStates(Enum):
+    # The client has pending upload the obligatory documents to plataform
+    PENDING_DOCUMENTS = auto()
+    # The quote has pending the review of documents by an operator
     PENDING_REVIEW = auto()
+    # The client has pending make the pay for the first service schedule
     PENDING_PAY = auto()
+    # The operator reject the documents upload by the client
     REJECT_BY_DOCUMENTS = auto()
+    # The pay of first service schedule is made by the client
     PAY_ACCEPTED = auto()
--- Services/States/ServicesState.py
+++ Services/States/ServicesState.py
@@ -3,5 +3,9 @@ from enum import Enum, unique, auto
 
 @unique
 class ServicesState(Enum):
+    # The service has been schedule for apply the service
     SCHEDULE = auto()
+    # The service is pending of schedule for apply the service
     UNSCHEDULED = auto()
+    # The client cancel the applying of recurring service
+    INACTIVE = auto()
